71173cc63f8b20c4ecba9fd42d3e3bba763084c3
DanielFranczak <dan.franczak@gmail.com>
2018-09-22 22:36:15 +0200

    First day finished! (I have to find quicker and easier solution

---
+++ largestNumInArr.js
@@ -0,0 +1,24 @@
+// Day 1 - return largest number in subarray in Array - Dzien 1 - zwroc najwieksza liczbę  w podtablicy Tablicy
+var arrNum = [
+    [123,212,144,521],
+    [600,222,654,112],
+    [872,1222,980,675],
+    [12,675,283,888],
+    [111,3292,887,998,97,12]
+];
+function largestOfFive(arr){
+    let result = [];
+    for (let i = 0; i < arr.length; i++){
+        let largestNum = arr[i][0];
+        for(let n = 1; n < arr[i].length; n++) {
+            if (arr[i][n] > largestNum){
+                largestNum = arr[i][n];
+            }
+        }
+        result[i] = largestNum;
+    }
+    return result;
+}
+
+
+console.log(largestOfFive(arrNum));
